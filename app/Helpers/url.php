2d3b385f632117adf1f70397cb0d642c3c229556
Pedro Leandro Gomes da Silva <pedroleandrogomesdasilva@outlook.com>
2026-09-03 15:02:57 -0300

    fix: corrigir path do assets_mazer para resources/themes/dist

--- app/Helpers/url.php
+++ app/Helpers/url.php
@@ -48,7 +48,7 @@ function assets(string $path = null): string
 
 function assets_mazer(string $path = null): string
 {
-    $base = appBaseUrl() . "/resources/themes/mazer-1.0.0/dist/assets";
+    $base = appBaseUrl() . "/resources/themes/dist";
 
     if ($path) {
         return $base . '/' . ltrim($path, '/');
